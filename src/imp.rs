9035cb7cb976bf0b0dab0e3682a854b106b6e583
Drew Crawford <drew@sealedabstract.com>
2025-02-15 03:09:12 -0600

    sketch wgpu backend

--- src/imp.rs
+++ src/imp.rs
@@ -1,5 +1,12 @@
 //at the moment we only support wgpu
 
+#[cfg(not(feature = "backend_wgpu"))]
 mod nop;
-
+#[cfg(not(feature = "backend_wgpu"))]
 pub use nop::*;
+
+#[cfg(feature = "backend_wgpu")]
+mod wgpu;
+
+#[cfg(feature = "backend_wgpu")]
+pub use wgpu::*;
