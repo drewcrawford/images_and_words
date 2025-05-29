bb4dbcc1718b2e37a967772a2631307fb7a01ce1
Drew Crawford <drew@sealedabstract.com>
2025-05-29 15:18:14 -0500

    Remove ResourceSpecialize

--- src/images/render_pass.rs
+++ src/images/render_pass.rs
@@ -1,34 +1,7 @@
-use std::collections::HashMap;
 use std::fmt::Debug;
 use crate::bindings::BindStyle;
 use crate::images::shader::{FragmentShader, VertexShader};
 
-/**
-This indicates a specialization we will use to hide resource access.
-
-For other forms of specialization, we should use another method,
-for details see obsidian://open?vault=mt2&file=IW%2FFunction%20speclialization
-
-# Metal
-
-On Metal, we specialize via... specialization
-
-# Vulkan
-
-On vulkan, we need to implement different entrypoints, mt2-270 and obsidian.
-
-*/
-#[derive(Debug,Clone)]
-pub struct ResourceSpecialize {
-    ///if there are more than 255 ways to specialize something we are fucked.
-    pub(crate) dict: HashMap<u8,bool>
-}
-impl ResourceSpecialize {
-    pub fn new() -> Self { Self { dict: HashMap::new()} }
-    pub fn set(&mut self, index: u8, value: bool) {
-        self.dict.entry(index).or_insert(value);
-    }
-}
 #[derive(Debug,Clone)]
 pub struct PassDescriptor {
     #[allow(dead_code)] //nop implementation does not use
@@ -37,8 +10,6 @@ pub struct PassDescriptor {
     pub(crate) vertex_shader: VertexShader,
     #[allow(dead_code)] //nop implementation does not use
     pub(crate) fragment_shader: FragmentShader,
-    #[allow(dead_code)] //todo: mt2-495
-    pub(crate) resource_specialize: ResourceSpecialize,
     pub(crate) bind_style: BindStyle,
     #[allow(dead_code)] //nop implementation does not use
     pub(crate) draw_command: DrawCommand,
@@ -54,13 +25,12 @@ impl PassDescriptor {
     /// `depth`: Whether to bind a depth texture to the render pass.
     /// ## Design note:
     /// We use Rust strings because we end up manipulating strings before passing to OS methods
-    pub fn new(name: String, vertex_shader: VertexShader, fragment_shader: FragmentShader, resource_specialize: ResourceSpecialize, bind_style: BindStyle,draw_command: DrawCommand,depth: bool, alpha: bool) -> Self {
+    pub fn new(name: String, vertex_shader: VertexShader, fragment_shader: FragmentShader, bind_style: BindStyle,draw_command: DrawCommand,depth: bool, alpha: bool) -> Self {
         Self {
             name,
             bind_style,
             vertex_shader,
             fragment_shader,
-            resource_specialize,
             draw_command,
             depth,
             alpha
@@ -77,8 +47,6 @@ impl PassDescriptor {
     }
     #[allow(dead_code)] //nop implementation does not use
     pub(crate) const fn bind_style(&self) -> &BindStyle { &self.bind_style }
-    #[allow(dead_code)] //mt2-495
-    pub(crate) const fn resource_specialize(&self) -> &ResourceSpecialize { &self.resource_specialize }
 }
 #[derive(Debug,Clone)]
 pub enum DrawCommand {
