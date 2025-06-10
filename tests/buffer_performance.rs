930012a396a38ab12f2935b0d0a679997bfa4552
Drew Crawford <drew@sealedabstract.com>
2025-06-10 03:16:55 -0500

    cargo fmt

--- tests/buffer_performance.rs
+++ tests/buffer_performance.rs
@@ -1,9 +1,9 @@
-use images_and_words::bindings::forward::dynamic::buffer::CRepr;
-use images_and_words::images::view::View;
-use images_and_words::images::projection::WorldCoord;
-use images_and_words::images::Engine;
 use images_and_words::bindings::forward::dynamic::buffer::Buffer;
+use images_and_words::bindings::forward::dynamic::buffer::CRepr;
 use images_and_words::bindings::visible_to::GPUBufferUsage;
+use images_and_words::images::Engine;
+use images_and_words::images::projection::WorldCoord;
+use images_and_words::images::view::View;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
